d3a50f3e6f2ef5d1679b83eec47e362c49326210
Gastondanielx4 <tochircentral@gmail.com>
2022-03-01 01:40:57 -0300

    primer commit

---
+++ js/color_header.js
@@ -0,0 +1,19 @@
+let d = document,
+    w = window;
+
+export default function colorearHeader (header){
+    
+    let $header = d.getElementById(header);
+
+    w.addEventListener("scroll", e =>{
+
+        let scrollTop = w.pageYOffset || d.documentElement.scrollTop;
+
+        if(scrollTop > 100) {
+            $header.classList.add("is-active")
+
+        }else{
+            $header.classList.remove("is-active")
+        }
+    });
+} 
---
+++ js/index_pokedex.js
@@ -0,0 +1,9 @@
+import colorearHeader from "/Pokemon/js/color_header.js";
+
+
+let d = document,
+    w = window;
+
+d.addEventListener("DOMContentLoaded", (e) => {
+    colorearHeader("headerStatic");
+});
